f8afffd784d86772a87ea952cab342d06ecded9a
Jonyxzx <jonykhozhixiong@gmail.com>
2024-06-28 23:29:39 +0800

    Add mongoDB connection

--- src/index.js
+++ src/index.js
@@ -11,6 +11,28 @@ root.render(
   </React.StrictMode>
 );
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin1:lYTwDHfVH9jLMC86@paytok.0rjgylm.mongodb.net/?retryWrites=true&w=majority&appName=PayTok";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
