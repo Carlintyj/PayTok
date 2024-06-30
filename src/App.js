3d1c810ef43388bf8e41836df7996375953e9d2e
Ong Shun Ping <90850278+shunpingong@users.noreply.github.com>
2024-06-30 19:14:16 +0800

    Add screenflow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import PaymentPage from "./pages/PaymentPage";
+import Profile from "./pages/Profile";
+import Transactions from "./pages/Transactions";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/transaction" element={<Transactions />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/PaymentPage.js
@@ -0,0 +1,12 @@
+/**
+ * A component for displaying payment page UI. (Transfer to merchant / Other people)
+ * @component
+ * @returns {JSX.Element} Payment Page UI.
+ */
+export default function PaymentPage() {
+  return (
+    <div>
+      <h1>Payment Page</h1>
+    </div>
+  );
+}
