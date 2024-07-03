37edb9949f5e6d33a3781311c56e190d8498041a
AaronLim2001 <127822025+AaronLim2001@users.noreply.github.com>
2024-07-03 23:40:46 +0800

    Update PaymentPage, SuccessfulPaymentPage, and UnsuccessfulPaymentPage imports

--- src/pages/SuccessfulPaymentPage.js
+++ src/pages/SuccessfulPaymentPage.js
@@ -1,15 +1,6 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper"
-import TextField from "@mui/material/TextField";
-import InputAdornment from '@mui/material/InputAdornment';
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Button, Typography, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import Container from "@mui/material/Container";
 
 export default function SuccessfulPaymentPage() {
   const navigate = useNavigate();
